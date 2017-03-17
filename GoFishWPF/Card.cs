b6ea469529c6bfe7e63f9f294e51ae8775eea8dd
radu_atl <radumamut@yahoo.com>
2017-03-17 09:59:06 -0400

    Frist commit of GoFish WPF

---
+++ GoFishWPF/Card.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoFishWPF
+{
+
+    enum Suits { Spades, Clubs, Diamonds, Hearts }  // ♠  , ♣ , ♦ , ♥ (spades, clubs, diamonds, harts)
+
+    enum Values
+    {
+        Ace = 1, Two = 2, Three = 3, Four = 4, Five = 5, Six = 6, Seven = 7, Eight = 8,
+        Nine = 9, Ten = 10, Jack = 11, Queen = 12, King = 13
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    class Card {
+        public Suits Suit { get; set; }
+        public Values Value { get; set; }
+
+        public Card(Suits suit, Values value)
+        {
+            Suit = suit;
+            Value = value;
+        }
+        public string Name
+        {
+            get { return Value.ToString() + " of " + Suit.ToString(); }
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public static bool DoesCardMatch(Card cardToCheck, Suits suit)
+        {
+            if (cardToCheck.Suit == suit)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        public static bool DoesCardMatch(Card cardToCheck, Values value)
+        {
+            if (cardToCheck.Value == value)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        public static string Plural(Values value)
+        {
+            if (value == Values.Six)
+                return "Sixes";
+            else
+                return value.ToString() + "s";
+        }
+
+    }
+}
---
+++ GoFishWPF/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace GoFishWPF {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        Game game;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            game = this.FindResource("game") as Game;
+        }
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            game.StartGame();
+        }
+
+        private void askForACard_Click(object sender, RoutedEventArgs e)
+        {
+            if (cards.SelectedIndex >= 0)
+                game.PlayOneRound(cards.SelectedIndex);
+        }
+
+        private void cards_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (cards.SelectedIndex >= 0)
+                game.PlayOneRound(cards.SelectedIndex);
+        }
+
+
+    }
+}
